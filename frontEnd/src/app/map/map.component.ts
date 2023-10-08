60977b3e13a7885f35629df171381c04a32a1996
Abray0 <rayesabdou0@gmail.com>
2023-10-08 02:21:12 +0100

    citizens app hh done

--- frontEnd/src/app/map/map.component.ts
+++ frontEnd/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,Input } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-editable';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +10,8 @@ import * as L from 'leaflet';
 })
 export class MapComponent  implements OnInit {
   @ViewChild('map', { static: true }) mapElement!: ElementRef;
+  @Input() showCircle: boolean = false;
+  @Input() showpoligon: boolean = false;
 
   private map!: L.Map;
 
@@ -17,19 +20,46 @@ export class MapComponent  implements OnInit {
   }
 
   private initializeMap() {
-    this.map = L.map(this.mapElement.nativeElement).setView([35.7596, -5.8330], 13);
+    this.map = L.map('map').setView([35.7596, -5.8330], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors',
     }).addTo(this.map);
 
-    const tangierCoordinates: [number, number] = [35.7596, -5.8330];
-    this.addPin(tangierCoordinates);
-  }
+    const Coordinates: [number, number] = [35.7596, -5.8330];
 
-  private addPin(coordinates: [number, number]) {
-    L.marker(coordinates, { zIndexOffset: 1000 }).addTo(this.map);
+    // Add a marker
+    L.marker(Coordinates, { zIndexOffset: 1000, draggable: false }).addTo(this.map);
+
+    if (this.showCircle) {
+      // Add a circle
+      const circle = L.circle(Coordinates, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.5,
+        radius: 500, // Adjust the radius as needed (in meters)
+      }).addTo(this.map);
+
+      const circle1 = L.circle(Coordinates, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.5,
+        radius: 1000, // Adjust the radius as needed (in meters)
+      }).addTo(this.map);
+    }
+    
+
+      /* if(this.showpoligon){
+        // Add a polygon
+        const polygon = L.polygon([
+          [35.8096, -5.8430],
+          [35.7590, -5.7955],
+          [35.7585, -5.8340]
+        ], {
+          color: 'red',
+          fillColor: 'red',
+          fillOpacity: 0.5
+        }).addTo(this.map);
+      } */
   }
-  
-  constructor() { }
 }
